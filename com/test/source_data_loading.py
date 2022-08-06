d65506a5a860cd0a992d2a11d3ade2e5ba5a07ff
n.k.hareesh@gmail.com <Vijitha918!>
2022-08-06 21:12:07 +0530

    initial commit

--- com/test/source_data_loading.py
+++ com/test/source_data_loading.py
@@ -10,8 +10,8 @@ if __name__ == '__main__':
     )
 
     current_dir = os.path.abspath(os.path.dirname(__file__))
-    app_config_path = os.path.abspath(current_dir + "/../../../../" + "application.yml")
-    app_secrets_path = os.path.abspath(current_dir + "/../../../../" + ".secrets")
+    app_config_path = os.path.abspath(current_dir + "/../../" + "application.yml")
+    app_secrets_path = os.path.abspath(current_dir + "/../../" + ".secrets")
 
     conf = open(app_config_path)
     app_conf = yaml.load(conf, Loader=yaml.FullLoader)
@@ -83,4 +83,4 @@ if __name__ == '__main__':
                 .mode("overwrite") \
                 .parquet("s3a://" + app_conf["s3_conf"]["s3_bucket"] + '/' + staging_loc + "/" + src)
 
-# spark-submit --packages "mysql:mysql-connector-java:8.0.15" dataframe/ingestion/others/systems/mysql_df.py
+# spark-submit --packages "org.apache.hadoop:hadoop-aws:2.7.4,mysql:mysql-connector-java:8.0.15,org.mongodb.spark:mongo-spark-connector_2.11:2.4.1" com/test/source_data_loading.py
